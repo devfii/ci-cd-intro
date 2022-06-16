26a4752b054149039f8b5d54d929b4cc3f804878
Emmanuel Koomson <devfeefe@gmail.com>
2022-06-16 22:46:57 +0000

    updated route

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Nimble-ICE' })
-})
+router.get("/users", function (req, res, next) {
+  res.render("index", { title: "Nimble-ICE" });
+});
 
 /* GET ice-flakes resource */
-router.get('/ice-flakes', function (req, res, next) {
-  res
-    .status(201)
-    .json({
-      resource: 'ice-flakes',
-      count: 205,
-      shape: 'circle'
-    })
-})
+router.get("/ice-flakes", function (req, res, next) {
+  res.status(201).json({
+    resource: "ice-flakes",
+    count: 205,
+    shape: "circle",
+  });
+});
 
-module.exports = router
+module.exports = router;
